84d68c2395d9eca97e9459eba893982fa3dc2f2d
Carolina Hadad <carolinahadad@gmail.com>
2015-11-23 20:55:57 -0300

    Redondeo grados centigrados y agrego unidades

--- client/code/app/clima.js
+++ client/code/app/clima.js
@@ -2,9 +2,9 @@
 ss.event.on('estadoclima', function(message) {
 	var clima_externo = message;
 console.log (clima_externo);
-	clima_externo.temperatura = clima_externo.temperatura.toFixed(1);
-  clima_externo.temperaturaMax = clima_externo.temperaturaMax.toFixed(1);
-  clima_externo.temperaturaMin = clima_externo.temperaturaMin.toFixed(1);
+	clima_externo.temperatura = clima_externo.temperatura.toFixed(0);
+  clima_externo.temperaturaMax = clima_externo.temperaturaMax.toFixed(0);
+  clima_externo.temperaturaMin = clima_externo.temperaturaMin.toFixed(0);
   clima_externo.cielo = clima_externo.cielo.toLowerCase().replace(/\s/g,"");
 
 	var html = ss.tmpl['widgets-clima'].render({
--- client/code/app/climainterno.js
+++ client/code/app/climainterno.js
@@ -1,8 +1,11 @@
 'use strict';
 
 ss.event.on('climainterno', function(message) {
+
+    var clima_interno = Math.round(message);
+
 	var html = ss.tmpl['widgets-climaInterno'].render({
-    	clima: message
+    	clima: clima_interno
 	});
 	return $("#climaInterno").html(html);
 });
